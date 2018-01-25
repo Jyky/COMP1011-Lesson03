2a9faefb23a9607d642f01203ff768c259bc0831
Jovin <Jovin@Jovinky>
2018-01-26 00:27:37 +0800

    Added vehicle class

--- src/Project.java
+++ src/Project.java
@@ -3,12 +3,14 @@
  */
 
 /**
- * @author Jovin
- *
+ * @author Jovin Yap
+ * Program Description: Demo Files for COMP1011 Lesson 03 
+ * @version 0.1 - Created basic Project
  */
 public class Project {
 
 	/**
+	 * Main Method for project
 	 * @param args
 	 */
 	public static void main(String[] args) {
---
+++ src/Vehicle.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jovin Yap
+ * Class Description: Creating a vehicle class to use as a superclass for all other vehhicles sub-classes
+ * 
+ */
+public class Vehicle {
+	int numWheels = 4;
+	int numDoors = 2;
+	String colour = "";
+	String make = "";
+	String model = "";
+	String year = "";
+	float speed = 0f;
+	
+	
+	public Vehicle(){
+		
+	}
+	
+	public void accelerate(){
+		
+	}
+	
+	public void decelerate(){
+		
+	}
+
+}
